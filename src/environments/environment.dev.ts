02bb02dc2ccc8977818302e53cecf49282d84726
Unknown <nattapat013@outlook.com>
2018-04-25 16:34:21 +0700

    edit setup

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://34.213.239.153:8082/graphql'
+  graphqlUri: 'http://34.213.239.153:8084/graphql'
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  graphqlUri: 'http://34.213.239.153:8082/graphql'
+  graphqlUri: 'http://34.213.239.153:8084/graphql'
 
 };
